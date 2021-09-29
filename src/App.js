39f2ef72ffbd2eef992f8dd4cd4f7433c5af3443
Bahere Zarezade <87176183+BahereZarezade@users.noreply.github.com>
2021-09-29 19:27:06 +0330

    first commit.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+                         <header className="App-header">
+        <div><h2>این اولین اپلیکیشن من با ری اکت است</h2></div>
       </header>
     </div>
   );
