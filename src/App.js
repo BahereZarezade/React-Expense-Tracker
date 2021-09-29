67a47d19e3a916e0acf4437e2824156277633e26
Bahere Zarezade <87176183+BahereZarezade@users.noreply.github.com>
2021-09-29 22:38:19 +0330

    commit.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function ExpenseItem() {
+  const expenseDate=new Date (2021,2,8);
+  const expenseTitle='بیمه ماشین';
+  const expenseAmount=294.67;
   return (
-    <div className="App">
-                         <header className="App-header">
-        <div><h2>این اولین اپلیکیشن من با ری اکت است</h2></div>
-      </header>
+    <div className="expense-item">
+       <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h3>{Math.random()}</h3>
+         <div className="expense-item__price">${expenseAmount}</div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default ExpenseItem;
