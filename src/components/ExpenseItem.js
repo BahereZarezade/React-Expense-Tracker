1ad0d74af9904a3c309aac3c85ab0ce0abc5641d
Bahere Zarezade <87176183+BahereZarezade@users.noreply.github.com>
2021-10-01 01:12:34 +0330

    commit.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,18 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate=new Date (2021,2,8);
-  const expenseTitle='بیمه ماشین';
-  const expenseAmount=294.67;
+function ExpenseItem(props) {
   return (
     <div className="container">
     <div className="expense-item">
-       <div className="date"><h2>{expenseDate.toISOString()}</h2>
-       <h2>{expenseTitle}</h2>
+      
+       <div className="date"><h2>{props.date.toISOString()}</h2>
+       <h2>{props.title}</h2>
        </div>
 
         <div className="expense-item__description">   
         <h3>{Math.random()}</h3>
-         <div className="expense-item__price">${expenseAmount}</div>
-         
+         <div className="expense-item__price">${props.amount}</div>
+
     </div>
     </div>
     </div>
