c96fb1f06b2829d25a5728c38197d828a88b959f
Bahere Zarezade <87176183+BahereZarezade@users.noreply.github.com>
2021-10-02 20:54:30 +0330

    commit.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,22 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-US', {month: 'long'});
+  const year = props.date.toLocaleString('en-US', {day: '2-digit'});
+  const day = props.date.getFullYear();
+
   return (
     <div className="container">
-    <div className="expense-item">
-      
-       <div className="date"><h2 className="date">{props.date.toISOString()}</h2>
-       <h2 className="tilte">{props.title}</h2>
-       </div>
-
-        <div className="expense-item__description">   
-        <h3>{Math.random()}</h3>
-         <div className="expense-item__price">${props.amount}</div>
-
+      <div className="expense-item">
+             <div>
+               <div>{month}</div>
+               <div>{day}</div>
+               <div>{year}</div>
+            </div>
+            <div><h2>{props.title}</h2></div>
+       <div className="expense-item__description">   
+       <h3>{Math.random()}</h3>
+       <div className="expense-item__price">${props.amount}</div>
     </div>
     </div>
     </div>
